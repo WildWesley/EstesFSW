31eab4d337de16e4bb3fa078b6026fc98d4499ea
WildWesley <86993555+WildWesley@users.noreply.github.com>
2022-04-05 17:44:36 -0500

    Commented on EstesFSW.ino

--- EstesFSW.ino
+++ EstesFSW.ino
@@ -3,10 +3,12 @@
 #include <TeensyThreads.h>
 #include <ArduinoQueue.h>
 
+// We begin by declaring a queue, renaming the Threads class from Mutex to mtx, and declaring a new bool that keeps track of when to send telemetry
 ArduinoQueue<Downlink> packets(20);
 Threads::Mutex mtx;
 volatile bool sendTelemetry = false;
 
+// We define our radio loop, which will wait until a packet is recieved, in which case we will tell the other methods to send telemetry with the sendTelemetry bool
 void radio_loop()
 {
   while(1)
@@ -43,6 +45,7 @@ void radio_loop()
   }
 }
 
+// This setup() method holds all the methods called in the setup phase
 void setup() {
   // put your setup code here, to run once:
   XBEE_SERIAL.begin(9600);
@@ -50,6 +53,8 @@ void setup() {
   radio.detach();
 }
 
+// This loop() method makes sure that hardware is ready and Telemetry needs to be sent, in which case it reads the data from the sensors and queues it up to be sent out
+// through the serial port
 void loop() {
   if (!Hardware::ready() || !sendTelemetry)
   {
